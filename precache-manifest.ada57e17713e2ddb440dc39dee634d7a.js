9d6f5db5461796339bc1966d2eefebefdea2cbc5
sinan <sinan.lakhani@hotmail.com>
2020-06-24 03:08:29 -0700

    Updates

---
+++ precache-manifest.ada57e17713e2ddb440dc39dee634d7a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5b5db7abdc6560560484bc39c4d6b02",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d906f5132118a4f2b0b",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "edbe7b50de56d79ee6d1",
+    "url": "/static/css/main.61b28c0f.chunk.css"
+  },
+  {
+    "revision": "3d906f5132118a4f2b0b",
+    "url": "/static/js/2.1ea09588.chunk.js"
+  },
+  {
+    "revision": "df835edbe6947b41c9ad692616389269",
+    "url": "/static/js/2.1ea09588.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edbe7b50de56d79ee6d1",
+    "url": "/static/js/main.d6fe429c.chunk.js"
+  },
+  {
+    "revision": "e93e8139225d8874858d",
+    "url": "/static/js/runtime-main.9a5e9a04.js"
+  },
+  {
+    "revision": "d44ee16c4c56b31517333e0d9b6293c1",
+    "url": "/static/media/ihorizon.d44ee16c.png"
+  },
+  {
+    "revision": "9305a29551b93a7aea398611aa88c5e6",
+    "url": "/static/media/logo1.9305a295.png"
+  },
+  {
+    "revision": "9042f8b7abc6afd0bb3702c817541030",
+    "url": "/static/media/logo2.9042f8b7.png"
+  },
+  {
+    "revision": "66a2fd0440dbebeabf47999e032a241a",
+    "url": "/static/media/logo3.66a2fd04.png"
+  },
+  {
+    "revision": "ffcf8cae240c8e928ebc75c4055bbacf",
+    "url": "/static/media/logo4.ffcf8cae.png"
+  }
+]);
